2feaab4a016b71fe5475cfc75b388d356cf645cc
jn-gautier <jean.nicolas.gautier@gmail.com>
2025-12-26 08:44:31 +0100

    Les thread de téléchargement sont maintenant gérés par une même fonction : set_download_thread

--- UI/import_from_drive.py
+++ UI/import_from_drive.py
@@ -1,41 +1,11 @@
 from PyQt6.QtWidgets import QInputDialog
-from PyQt6.QtWidgets import QProgressDialog
-
-from PyQt6.QtCore import QThread, Qt
-from UI.threads import Download_task
-
+from UI.set_download_thread import set_download_thread
 
 def dialog_import_download(self):
     if not self.sheets_id :
-        self.prog = QProgressDialog()
-        self.prog.setWindowFlags(Qt.WindowType.FramelessWindowHint)
-
-        self.prog.setCancelButton(None)
-        self.prog.setLabelText ('Téléchargement de la feuille...')
-        self.prog.setRange(0, 100) # Pour un barème simple
-        self.prog.setValue(0)
-        
-
-
         with open('./UI/config/sheet.id') as f:
             sheet_id = f.read().rstrip() 
-        
-        self.thread=QThread()
-        self.worker=Download_task()
-        self.worker.messagebox.connect(self.my_slots.show_messagebox)
-        self.worker.sheet_id=sheet_id
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
-        self.worker.progress.connect(self.my_slots.progress_dialog)
-        self.worker.finished.connect(self.my_slots.handle_sheets_id)
-        self.worker.finished.connect(self.thread.quit)
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.worker.failed.connect(self.thread.quit)
-        self.worker.failed.connect(self.worker.deleteLater)
-        self.worker.failed.connect(self.thread.deleteLater)
-        print(f"Téléchargement du document contenant les id des classes...")
-        self.thread.start()
+        set_download_thread(self,"main_sheet",sheet_id)
 
     classe,ok = QInputDialog.getItem(self,('Télécharger les points depuis Google Drive'),("Choisissez une classe"),['1 C a','1 C b','1 C c','1 C d','1 C e','1 C f','2 C a','2 C b','2 C c','2 C d','2 C e','2 C f','3 GT a','3 GT b','3 GT c','3 GT d','3 GT e','3 GT f','3 TQ','4 GT a','4 GT b','4 GT c','4 GT d','4 TQ','5 GT a','5 GT b','5 GT c','5 GT d','5 TQ','6 GT a','6 GT b','6 GT c','6 GT d','6 TQ'],editable = False)
     #classe=str(classe)
@@ -70,30 +40,8 @@ def get_sheet_classe(self,classe):
                break
         
         if sheet_id:
-            self.prog = QProgressDialog()
-            self.prog.setWindowFlags(Qt.WindowType.FramelessWindowHint)
-            self.prog.setCancelButton(None)
-            self.prog.setLabelText ('Téléchargement de la feuille...')
-            self.prog.setRange(0, 100) # Pour un barème simple
-            self.prog.setValue(0)
-            self.prog.show()
-
-            self.thread=QThread()
-            self.worker=Download_task()
-            self.worker.messagebox.connect(self.my_slots.show_messagebox)
-            self.worker.sheet_id=sheet_id
-            self.worker.moveToThread(self.thread)
-            self.worker.progress.connect(self.my_slots.progress_dialog)
-            self.thread.started.connect(self.worker.run)
-            self.worker.finished.connect(self.my_slots.handle_sheet_classe)
-            self.worker.finished.connect(self.thread.quit)
-            self.worker.finished.connect(self.worker.deleteLater)
-            self.thread.finished.connect(self.thread.deleteLater)
-            self.worker.failed.connect(self.thread.quit)
-            self.worker.failed.connect(self.worker.deleteLater)
-            self.worker.failed.connect(self.thread.deleteLater)
-            self.thread.start()
-            print(f"Téléchargement de la classe {classe}...")
+            set_download_thread(self,"classe_sheet",sheet_id,classe)
+            
         else:
             self.my_slots.show_messagebox("ID non trouvé", f"Aucun ID de feuille n'a été trouvé pour la classe : {classe_formattee}","warning")
                 
--- UI/set_download_thread.py
+++ UI/set_download_thread.py
@@ -1,31 +1,53 @@
 from PyQt6.QtWidgets import QProgressDialog
-from PyQt6.QtCore import Qt
-from PyQt6.QtCore import QObject, QThread, pyqtSignal as Signal
-from UI.threads import Compile_tableau_task
+from PyQt6.QtCore import Qt, QThread
+from UI.threads import Download_task
 
-def set_download_thread(self):
+
+def set_download_thread(self,task_name,sheet_id,classe=None):
+
+    #création du QProgressDialog
     self.prog = QProgressDialog()
     self.prog.setWindowFlags(Qt.WindowType.FramelessWindowHint)
     self.prog.setCancelButton(None)
-    self.prog.setLabelText ('Téléchargement de la feuille...')
+    
+    label="Téléchargement..."
+    if task_name=="main_sheet":
+        label="Téléchargement des id des classes..."
+    elif task_name=="classe_sheet":
+        label=f"Téléchargement du tableau de {classe}..."
+    self.prog.setLabelText (label)
     self.prog.setRange(0, 100) # Pour un barème simple
     self.prog.setValue(0)
     self.prog.show()
-
-    self.thread=QThread()
-    self.worker=Compile_tableau_task()
-    self.worker.messagebox.connect(self.my_slots.show_messagebox)
-    self.worker.chemin_fichier_tex=self.chemin_fichier_tex
     
+
+    #creation du thread
+    self.thread=QThread()
+    self.worker=Download_task()
+    self.worker.messagebox.connect(self.my_slots.show_messagebox)
+    self.worker.sheet_id=sheet_id
     self.worker.moveToThread(self.thread)
+    
+    #creation des connecteurs
     self.thread.started.connect(self.worker.run)
     self.worker.progress.connect(self.my_slots.progress_dialog)
-    #self.worker.finished.connect(self.my_slots.handle_sheets_id)
+    
+    
+    if task_name=="main_sheet":
+        self.worker.finished.connect(self.my_slots.handle_sheets_id)
+    elif task_name=="classe_sheet":
+        self.worker.finished.connect(self.my_slots.handle_sheet_classe)
+    
+
+    #creations des connecteurs de fin de tache
     self.worker.finished.connect(self.thread.quit)
     self.worker.finished.connect(self.worker.deleteLater)
     self.thread.finished.connect(self.thread.deleteLater)
+    
+    #creation des connecteurs d'échec
     self.worker.failed.connect(self.thread.quit)
     self.worker.failed.connect(self.worker.deleteLater)
-    self.worker.failed.connect(self.thread.deleteLater)
-    print(f"Compilation du document Latex...")
+    
+    #lancement du thread
+    print(label)
     self.thread.start()
